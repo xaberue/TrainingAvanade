fb4c5e7cf8769b249596380b6a1072a14aaf97d2
Xelit3 <xavi_abelaira@outlook.com>
2021-12-14 10:48:38 +0100

    Logging included in project

--- Training.WebAPI/Controllers/WeatherForecastController.cs
+++ Training.WebAPI/Controllers/WeatherForecastController.cs
@@ -25,6 +25,8 @@ namespace Training.WebAPI.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("GET WeatherForecast invoked");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
