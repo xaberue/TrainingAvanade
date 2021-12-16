37b7746971900b1345abb51c0479b85d794694da
Xelit3 <xavi_abelaira@outlook.com>
2021-12-16 21:32:28 +0100

    Restored template behaviour in Program.cs for CreateHostBuilder

--- Training.WebAPI/Program.cs
+++ Training.WebAPI/Program.cs
@@ -7,14 +7,17 @@ namespace Training.WebAPI
     {
         public static void Main(string[] args)
         {
-            Host.CreateDefaultBuilder(args)
-              .ConfigureWebHostDefaults(webBuilder =>
-              {
-                  webBuilder.UseStartup<Startup>();
-              })
-              .Build()
-              .Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
 
     }
 }
