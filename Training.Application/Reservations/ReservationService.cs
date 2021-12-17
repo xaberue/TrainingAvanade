408f7ad87a15936944c9061a285209fac53585a9
ivanete27 <ivan_272727@hotmail.com>
2021-12-17 10:38:26 +0100

    dto parameter name changed

--- Training.Application/Reservations/ReservationService.cs
+++ Training.Application/Reservations/ReservationService.cs
@@ -69,15 +69,15 @@ namespace Training.Application.Reservations
             };
         }
 
-        private Reservation MapDto(ReservationDto entity, Book book, User user)
+        private Reservation MapDto(ReservationDto dto, Book book, User user)
         {
             return new Reservation
             {
-                Id = entity.Id,
+                Id = dto.Id,
                 User = user,
                 Book = book,
-                CreationDate = entity.CreationDate,
-                ReturnDate = entity.ReturnDate,
+                CreationDate = dto.CreationDate,
+                ReturnDate = dto.ReturnDate,
             };
         }
     }
