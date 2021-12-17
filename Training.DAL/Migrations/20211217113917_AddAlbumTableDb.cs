761b969e89988767af6dc5b7ace443d109c1e517
ivanete27 <ivan_272727@hotmail.com>
2021-12-17 12:54:30 +0100

    Migration Album

---
+++ Training.DAL/Migrations/20211217113917_AddAlbumTableDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Training.DAL.Migrations
+{
+    public partial class AddAlbumTableDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
